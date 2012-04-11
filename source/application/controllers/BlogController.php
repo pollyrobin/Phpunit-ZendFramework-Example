013797132d0f6df1fcebb196cf04c74104f67852
daan <daan@impres.nl>
2012-04-11 12:03:43 +0200

    Edited test classes so that there is 100% coverage

--- source/application/controllers/BlogController.php
+++ source/application/controllers/BlogController.php
@@ -28,6 +28,7 @@ class BlogController extends Zend_Controller_Action
         
         if (null === $blog) {
             $this->_redirect('blog');
+            return;
         }
 
         $this->view->blog = $blog;
@@ -54,37 +55,11 @@ class BlogController extends Zend_Controller_Action
                 $blogModel->save($data);
 
                 $this->_redirect('blog');
+                return;
             }
         }
         $this->view->form = $form;
     }
-    /**
-     * copy pasta van de methode hierboven
-	 * met als hoofdreden het testen van phpcpd 
-     *
-     * @return void
-     */
-    public function addcpdAction()
-    {
-        $form = new Application_Form_Blog();
-        if ($this->getRequest()->isPost()) {
-            $data = $this->getRequest()->getPost();
-            if ($form->isValid($data)) {
-                $data = array(
-                    'title' => $data['title'],
-                    'text' => $data['text'],
-                    'date_created' => new Zend_Db_Expr('NOW()'),
-                );
-
-                $blogModel = new Application_Model_DbTable_Blog();            
-                $blogModel->save($data);
-
-                $this->_redirect('blog');
-            }
-        }
-        $this->view->form = $form;
-    }
-
 
     /**
      * Edit an blog with the newly given user data
@@ -101,6 +76,7 @@ class BlogController extends Zend_Controller_Action
 
         if (null === $blog) {
             $this->_redirect('blog');
+            return;
         }
         
         $form = new Application_Form_Blog();
@@ -119,6 +95,7 @@ class BlogController extends Zend_Controller_Action
                 $blogModel->save($data, $id);
 
                 $this->_redirect('blog');
+                return;
             }
         }
         $this->view->form = $form;
--- source/tests/application/controllers/BlogControllerTest.php
+++ source/tests/application/controllers/BlogControllerTest.php
@@ -2,6 +2,18 @@
 
 class BlogControllerTest extends ControllerTestCase 
 {
+
+    /**
+     * Checks if the index page can be loaded
+     *
+     * @return void
+     */
+    public function testIndexAction()
+    {
+        $this->disPatch('/blog/index/');
+        $this->assertResponseCode(200);
+    }
+
     /**
      * does an request to the detail action
      * checks if the code 200 is returned
@@ -15,19 +27,123 @@ class BlogControllerTest extends ControllerTestCase
             ->setMethod('GET');
         $this->disPatch('/blog/detail/');
 
-        echo $this->getResponse()->getBody();
-
         $this->assertResponseCode(200);
     }     
 
     /**
-     * Checks if the index page can be loaded
+     * Tests if the user gets redirected when the detail page does not exist
      *
      * @return void
      */
-    public function testIndexAction()
+    public function testRedirectWhenDetailNotExists()
     {
-        $this->disPatch('/');
-        $this->assertResponseCode(200);
+        $this->getRequest()
+            ->setParams(array('id' => '100213'))
+            ->setMethod('GET');
+        $this->disPatch('/blog/detail/');
+
+        $this->assertEquals($this->getResponse()->getBody(), '');
+    }
+
+    /**
+     * Tests if a form submit can be made with valid data
+     *
+     * @return void
+     */
+    public function testAddActionPostValidValues()
+    {
+        $this->getRequest()->setMethod('POST'); 
+        $this->getRequest()
+            ->setPost(array(
+                'title' => 'test',
+                'text' => 'text',
+                'date_created' => new Zend_Db_Expr('NOW()')
+            ));
+        $this->disPatch('/blog/add/');
+
+        $this->assertEquals($this->getResponse()->getBody(), '');
+    }
+
+    /**
+     * Test if the form actually fails on invalid data
+     *
+     * @return void
+     */
+    public function testAddActionPostInvalidValues()
+    {
+        $this->getRequest()->setMethod('POST'); 
+        $this->getRequest()
+            ->setPost(array(
+                'blaat' => 'test',
+                'blaat' => 'text',
+                'blaat' => new Zend_Db_Expr('NOW()')
+            ));
+        $this->disPatch('/blog/add/');
+
+        $this->assertQuery('h1', 'Blog toevoegen'); 
+    }
+
+    /**
+     * Test if the redirect works if the id is incorrect
+     * 
+     * @return void
+     */
+    public function testEditActionRedirectWhenNotExists()
+    {
+        $this->getRequest()
+            ->setParams(array('id' => '100213'))
+            ->setMethod('GET');
+        $this->disPatch('/blog/edit/');
+
+        $this->assertEquals($this->getResponse()->getBody(), '');
+    }
+
+    /**
+     * Tests if the user can edit an blog item
+     *
+     * @return void
+     */
+    public function testEditActionPostValidValues()
+    {
+        $this->getRequest()
+            ->setParams(array('id' => '1'))
+            ->setMethod('GET');
+        $this->disPatch('/blog/edit/');
+
+        $this->getRequest()->setMethod('POST'); 
+        $this->getRequest()
+            ->setPost(array(
+                'title' => 'test',
+                'text' => 'text',
+                'date_created' => new Zend_Db_Expr('NOW()')
+            ));
+        $this->disPatch('/blog/edit/');
+
+        $this->assertQuery('h1', 'Blog aanpassen');  
+    }
+
+    /**
+     * Test if the user can't enter empty or wrong values
+     *
+     * @return void
+     */
+    public function testEditActionPostInvalidValues()
+    {
+        $this->getRequest()->setMethod('GET'); 
+        $this->getRequest()
+            ->setParams(array('id' => '1'));
+
+        $this->disPatch('/blog/edit/');
+
+        $this->getRequest()->setMethod('POST');
+        $this->getRequest()
+            ->setPost(array(
+                'bla' => '',
+                'text' => 'this text was edited by phpunit',
+                'date_created' => new Zend_Db_Expr('NOW()')
+            ));
+        $this->disPatch('/blog/edit/');
+
+        $this->assertQuery('h1', 'Blog toevoegen');  
     }
 }
